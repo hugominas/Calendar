3577981ed160c99fd885207acad06931fb0e06f0
Hugo Rodrigues <hugo.rodrigues@hiperformancesales.com>
2016-08-04 14:14:19 +0100

    update structure and gulp file

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const watchify    = require('watchify');
+const babelify    = require('babelify');
+const browserify  = require('browserify');
+const gulp        = require('gulp');
+const source      = require('vinyl-source-stream');
+const buffer      = require('vinyl-buffer');
+const gutil       = require('gulp-util');
+const uglify      = require('gulp-uglify');
+const browserSync = require('browser-sync');
+const runSequence = require('run-sequence');
+const imagemin    = require('gulp-imagemin');
+const pngquant    = require('imagemin-pngquant');
+const del         = require('del');
+const htmlmin     = require('gulp-htmlmin');
+const plumber     = require('gulp-plumber');
+const cleanCSS    = require('gulp-clean-css');
+
+// add options to babelify
+var options             = watchify.args;
+    options.entries     = 'src/assets/js/client.js';
+    options.extensions  = ['.js'];
+
+
+// prepare browserify object
+var w = watchify(browserify(options)).transform(babelify.configure({
+        presets: ["es2015", "react"],
+        ignore: /(bower_components)|(node_modules)/
+    }));
+    w.on('update', bundle);
+    w.on('log', gutil.log);
+
+
+// builder function
+function bundle() {
+    return w.bundle()
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        .pipe(source('dist/assets/js/bundle.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest(''));
+}
+
+// task and listeners
+gulp.task('browserify', bundle);
+
+/**
+ * This task removes all files inside the 'dist' directory.
+ */
+gulp.task('clean', () =>
+{
+    del.sync('./dist/**/*');
+});
+
+
+/**
+ * This task starts browserSync. Allowing refreshes to be called from the gulp
+ * bundle task.
+ */
+gulp.task('browser-sync', () =>
+{
+    return browserSync({ server:  { baseDir: './dist' } });
+});
+
+/**
+ * This task will remove all unecessary CSS lines
+ */
+ gulp.task('fonts', () => {
+     return gulp.src('./src/assets/fonts/**')
+         .pipe(gulp.dest('./dist/assets/fonts/'));
+ });
+ /**
+ * This task will copy index.html into 'dist'.
+ * If you want to process it, just add your code to this task.
+ */
+gulp.task('html', () =>
+{
+    return gulp.src(['./src/*.html'])
+        .pipe(plumber())
+        //.pipe(validator())
+        .pipe(htmlmin())
+        .pipe(gulp.dest('./dist/'));
+});
+ /**
+  * This task will copy all files from libs into 'dist/img'.
+  * If you want to process them, just add your code to this task.
+  */
+ gulp.task('img', () =>
+ {
+     return gulp.src(['./src/assets/img/**'])
+         .pipe(plumber())
+         .pipe(imagemin({
+     			progressive: true,
+     			svgoPlugins: [{removeViewBox: false}],
+     			use: [pngquant()]
+     		}))
+         .pipe(gulp.dest('./dist/assets/img'))
+ });
+
+ /**
+ * This task will copy all files from libs into 'dist/css'.
+ * If you want to process them, just add your code to this task.
+ */
+gulp.task('css', () =>
+{
+    return gulp.src(['./src/assets/css/**'])
+        .pipe(plumber())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist/assets/css'))
+});
+
+
+
+gulp.task('default', (callback)=> {
+  runSequence('clean', 'html', 'css', 'fonts', 'img', 'browserify', 'browser-sync');
+});
